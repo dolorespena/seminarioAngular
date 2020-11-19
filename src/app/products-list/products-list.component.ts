c8b2a63b0310f92916245328d127725d74b5da2b
Dolores Pena <dolorespenadecristofaro@gmail.com>
2020-11-19 20:38:52 -0300

    incorporacion de elementos en tabla

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-productss-list',
-  templateUrl: './productss-list.component.html',
-  styleUrls: ['./productss-list.component.scss']
+  selector: 'app-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.scss']
 })
 export class ProductssListComponent implements OnInit {
 
+  products = {
+    "image":"assets/img/cuadernillo.jpeg",
+    "product":"Agenda",
+    "type":"Personalizada",
+    "price": 1200,
+    "stock": 2
+  }
   constructor() { }
 
   ngOnInit(): void {
