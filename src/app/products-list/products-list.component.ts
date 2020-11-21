4353ee19327b059327b624b92a1851015b05c920
Dolores Pena <dolorespenadecristofaro@gmail.com>
2020-11-21 20:09:32 -0300

    ngIf sin Stock

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -12,31 +12,35 @@ export class ProductsListComponent implements OnInit {
   products: Products[] = [
     {
       image:'assets/img/cuadernillo.jpeg',
-      product:'Agenda',
-      type:'Personalizada',
-      price: 1200,
-      stock: 2
-    },
-    {
-      image:'assets/img/cuadernillo.jpeg',
       product:'Cuadernillo',
       type:'Estandar',
       price: 650,
-      stock: 0
+      stock: 0,
+      clearance: false,
+    },
+    {
+      image:'assets/img/cuadernillo.jpeg',
+      product:'Agenda',
+      type:'Personalizada',
+      price: 1200,
+      stock: 2,
+      clearance: true,
     },
     {
       image:'assets/img/cuadernillo.jpeg',
       product:'Anotadores',
       type:'Estandar',
       price: 250,
-      stock: 8
+      stock: 8,
+      clearance: false,
     },
     {
       image:'assets/img/cuadernillo.jpeg',
       product:'Tarjetas personales',
       type:'Personalizada',
       price: 300,
-      stock: 0
+      stock: 0,
+      clearance: false, 
     }
   ];
   
--- src/app/products-list/products.ts
+++ src/app/products-list/products.ts
@@ -4,4 +4,5 @@ export interface Products{
     price: number;
     stock: number;
     image: string;
+    clearance: boolean;
 }
