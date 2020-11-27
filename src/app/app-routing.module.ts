d5ea900f346dd8734ba56434a56ef39f3b7409b6
Dolores Pena <dolorespenadecristofaro@gmail.com>
2020-11-26 21:27:53 -0300

    ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AtelierContactComponent } from './atelier-contact/atelier-contact.component';
+import { AtelierProductsComponent } from './atelier-products/atelier-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'products',
+    pathMatch: 'full'
+  },
+  {
+    path: 'products', 
+    component: AtelierProductsComponent
+  },
+  {
+    path: 'contact', 
+    component: AtelierContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
